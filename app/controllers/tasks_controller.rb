a63f891a707bc5ce6037da020a7085e0442fb898
maro <amoebamaro@gmail.com>
2019-11-10 17:17:13 +0900

    edit, update, destroy実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,7 +16,32 @@ class TasksController < ApplicationController
         render 'new'
       end
     end
+
+    def show
+      @task = Task.find(params[:id])
+    end
+
+    def edit
+      @task = Task.find(params[:id])
+    end
   
+    def update
+      @task = Task.find(params[:id])
+  
+      if @task.update(task_params)
+        redirect_to tasks_path
+      else
+        render 'edit'
+      end
+    end
+    
+    def destroy
+      @task = Task.find(params[:id])
+  
+      @task.destroy
+      redirect_to tasks_path
+    end
+
     private
     def task_params
       params.require(:task).permit(:title, :description)
